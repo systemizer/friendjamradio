a9f6e6712452247a912fbd364f68ff1efaa66708
Rob McQueen <rob@nylas.com>
2015-07-18 11:57:22 -0700

    Autoplay when clicking play button

--- web/static/web/js/script.js
+++ web/static/web/js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   $('.play-track').click(function() {
     var trackId = $(this).data("trackid");
-    $(".player iframe").attr("src", "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/" + trackId + "&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true");
+    $(".player iframe").attr("src", "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/" + trackId + "&amp;auto_play=true&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true");
   });
 })
