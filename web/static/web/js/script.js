d95bbaae6292b883855bf19f7e3f2b215f552f54
Rob McQueen <rob@nylas.com>
2015-07-18 11:56:32 -0700

    Basic interface

--- web/static/web/js/script.js
+++ web/static/web/js/script.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
-  console.log("HELLO WORLD");
-
+  $('.play-track').click(function() {
+    var trackId = $(this).data("trackid");
+    $(".player iframe").attr("src", "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/" + trackId + "&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true");
+  });
 })
