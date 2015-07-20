19ad65f85912d4733cb4c81524b1ecf9c21aa0ca
Rob McQueen <rob@nylas.com>
2015-07-19 21:50:39 -0700

    Refresh soundcloud token if httperror

--- web/utils.py
+++ web/utils.py
@@ -1,6 +1,27 @@
 import soundcloud
+from requests.exceptions import HTTPError
+from social.apps.django_app.utils import load_strategy
 
+class DjangoSoundcloudClient(soundcloud.Client):
+    """
+    Simple Wrapper Around Soundcloud Client that will
+    refresh the access token if it receives a 401
+    """
 
-def soundcloud_client_for_user(user):
-    return soundcloud.Client(
-        access_token=user.social_auth.all()[0].access_token)
+    def __init__(self, django_user, *args, **kwargs):
+        self.django_user = django_user
+        super(DjangoSoundcloudClient, self).__init__(
+            access_token = django_user.social_auth.get().access_token,
+            *args, **kwargs)
+
+    def __getattr__(self, name, **kwargs):
+        """Translate an HTTP verb into a request method."""
+        try:
+            return super(DjangoSoundcloudClient, self).__getattr__(name,
+                                                                   **kwargs)
+        except HTTPError:
+            strategy = load_strategy()
+            django_user.social_auth.get().refresh_token(strategy)
+        finally:
+            return super(DjangoSoundcloudClient, self).__getattr__(name,
+                                                                   **kwargs)
--- web/views.py
+++ web/views.py
@@ -3,14 +3,14 @@ from django.contrib.auth.models import User
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
 
-from web.utils import soundcloud_client_for_user
+from web.utils import DjangoSoundcloudClient
 
 
 @login_required
 def index(request):
     userid = request.GET.get("userid") or request.user.id
     user = get_object_or_404(User, pk=userid)
-    client = soundcloud_client_for_user(user)
+    client = DjangoSoundcloudClient(user)
 
     tracks = [s['origin'] for s in
               client.get("/me/activities", limit=50).obj['collection']]
